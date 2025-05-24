f1553bbfd60179d0d724c96ab30adf8762617d16
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2025-05-25 06:42:34 +0800

    volume adjustment

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -10,14 +10,19 @@ public class VolumeSettings : MonoBehaviour
 
     [SerializeField] AudioSource BGMSource;
     [SerializeField] AudioSource SFXSource;
+    [SerializeField] private AudioClip testSFX; // Add this field for test sound
+
 
     private bool isMusicMuted = false;
     private bool isSFXMuted = false;
+    private bool isAdjusting = false;
+
 
     void Start()
     {
         SetMusicVolume();
-        SetSFXVolume();
+        float SFXVolume = SFXSlider.value;
+        audioMixer.SetFloat("SFX", Mathf.Log10(SFXVolume) * 20);
     }
 
     public void ToggleMusic()
@@ -37,7 +42,6 @@ public class VolumeSettings : MonoBehaviour
         SFXSlider.interactable = !isSFXMuted;
     }
 
-
     public void SetMusicVolume()
     {
         float BGMVolume = BGMSlider.value;
@@ -48,5 +52,20 @@ public class VolumeSettings : MonoBehaviour
     {
         float SFXVolume = SFXSlider.value;
         audioMixer.SetFloat("SFX", Mathf.Log10(SFXVolume) * 20);
+
+        // Play test sound only once when starting to adjust
+        if (!isAdjusting && !isSFXMuted)
+        {
+            SFXSource.PlayOneShot(testSFX);
+            isAdjusting = true;
+
+            // Reset the flag after a short delay
+            Invoke("ResetAdjusting", 1f);
+        }
+    }
+
+    private void ResetAdjusting()
+    {
+        isAdjusting = false;
     }
 }
