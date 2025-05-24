cea800857b860ccb834e6528442f1e01fc425298
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2025-05-25 03:09:12 +0800

    proper faders

--- Assets/Scripts/PanelFader.cs
+++ Assets/Scripts/PanelFader.cs
@@ -1,10 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class PanelFader : MonoBehaviour
 {
     public float fadeDuration = 0.5f;
+    private const float FADE_DELAY = 0.5f;
 
     public void FadeIn(CanvasGroup canvasGroup)
+    {
+        StartCoroutine(FadeInCoroutine(canvasGroup));
+    }
+
+    private IEnumerator FadeInCoroutine(CanvasGroup canvasGroup)
+    {
+        yield return new WaitForSeconds(FADE_DELAY);
+        FadeInDelayed(canvasGroup);
+    }
+
+    private void FadeInDelayed(CanvasGroup canvasGroup)
     {
         canvasGroup.gameObject.SetActive(true);
         LeanTween.alphaCanvas(canvasGroup, 1f, fadeDuration)
