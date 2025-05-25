efd002001366b6676981de5ae3c5657d1bd3c330
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2025-05-26 01:29:01 +0800

    choose race

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -98,26 +98,26 @@ public class CharacterStats : MonoBehaviour
         {
             case ROLE.CAPTAIN:
                 SetBaseStats(11, 13, 3, 0, 1);
-                eloquence += 2;
-                stealth += 1;
+                eloquence = 2;
+                stealth = 1;
                 primaryWeapon = new Weapon("Combat Knife", MODIFIER.STR, 1, DICE.D6, WEAPON_TYPE.MELEE, DAMAGE_TYPE.PHYSICAL);
                 break;
             case ROLE.REAPER:
                 SetBaseStats(10, 12, 0, 3, 2);
-                domination += 2;
-                acrobatics += 1;
+                domination = 2;
+                acrobatics = 1;
                 primaryWeapon = new Weapon("Light Pistol", MODIFIER.DEX, 1, DICE.D6, WEAPON_TYPE.RANGED, DAMAGE_TYPE.KINETIC);
                 break;
             case ROLE.ENGINEER:
                 SetBaseStats(12, 14, 2, 1, 0);
-                technology += 2;
-                athletics += 1;
+                technology = 2;
+                athletics = 1;
                 primaryWeapon = new Weapon("Reinforced Gauntlet", MODIFIER.STR, 1, DICE.D6, WEAPON_TYPE.MELEE, DAMAGE_TYPE.PHYSICAL);
                 break;
             case ROLE.SURGEON:
                 SetBaseStats(9, 11, 1, 2, 3);
-                sensory += 2;
-                database += 1;
+                sensory = 2;
+                database = 1;
                 primaryWeapon = new Weapon("Injector Pistol", MODIFIER.INT, 1, DICE.D4, WEAPON_TYPE.RANGED, DAMAGE_TYPE.PHYSICAL);
                 break;
         }
@@ -133,9 +133,50 @@ public class CharacterStats : MonoBehaviour
         intelligence = baseINT;
     }
 
-    public void ChooseRace()
+    public void ChooseRace(RACE race)
     {
-
+        switch (race)
+        {
+            case RACE.EARTHKIN:
+                strength += 1;
+                dexterity += 1;
+                intelligence += 1;
+                eloquence += 2;
+                instinct += 1;
+                break;
+            case RACE.CYBORG:
+                intelligence += 2;
+                technology += 2;
+                sensory += 1;
+                break;
+            case RACE.STEELFORGED:
+                strength += 1;
+                database += 2;
+                technology += 1;
+                break;
+            case RACE.DEMONOID:
+                strength += 2;
+                domination += 2;
+                eloquence += 1;
+                break;
+            case RACE.HEXARI:
+                dexterity += 1;
+                instinct += 2;
+                athletics += 1;
+                break;
+            case RACE.DRAKO:
+                hpMax += 2;
+                hpCurrent = hpMax;
+                acTotal += 1;
+                athletics += 2;
+                acrobatics += 1;
+                break;
+            case RACE.XENTHARIAN:
+                dexterity += 2;
+                stealth += 2;
+                domination += 1;
+                break;
+        }
     }
 
     public void PlayAsEien()
