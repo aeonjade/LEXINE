1da2bf09d6331d20724601fed83d1bdc727b0953
xJaDe16x <aeonjade.vicentuan16@gmail.com>
2025-05-28 05:55:30 +0800

    json partially working

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,10 +1,69 @@
 using System;
 using TMPro;
 using UnityEngine;
+using System.IO;
 
 [Serializable]
 public class CharacterStats : MonoBehaviour
 {
+    [Serializable]
+    private class WeaponData
+    {
+        public string weaponName;
+        public MODIFIER modifier;
+        public int rollNumber;
+        public DICE dice;
+        public WEAPON_TYPE weaponType;
+        public DAMAGE_TYPE damageType;
+    }
+
+    [Serializable]
+    private class ArmorData
+    {
+        public string armorName;
+        public int additionalArmorClass;
+    }
+
+    [Serializable]
+    private class AugmentData
+    {
+        public string augmentName;
+        public string augmentType;
+        public int cooldownAmount;
+        public string cooldownInterval;
+        public string augmentDescription;
+    }
+
+    [Serializable]
+    private class SaveData
+    {
+        // Basic Info
+        public string raiderName;
+        public ROLE raiderRole;
+        public RACE raiderRace;
+        public ALIGNMENT alignment;
+        public string looks1, looks2, looks3;
+        public string attitude1, attitude2, attitude3;
+        public POSITION position;
+        public string story;
+
+        // Equipment
+        public WeaponData primaryWeapon;
+        public WeaponData secondaryWeapon;
+        public ArmorData armor;
+        public AugmentData coreAugment;
+
+        // Stats & Skills
+        public int hpMax, hpCurrent, acTotal, acTemp;
+        public int strength, intelligence, dexterity, credits;
+        public int acrobatics, athletics, database, domination;
+        public int eloquence, instinct, sensory, stealth, technology;
+
+        // Ship Parts & Appearance
+        public bool hasPowerRegulator, hasMainDrive, hasStabilizerUnit;
+        public bool hasNavigationModule, hasShieldGenerator, hasType2Portrait;
+    }
+
     public string raiderName;
     public ROLE raiderRole;
     public RACE raiderRace;
@@ -236,6 +295,165 @@ public class CharacterStats : MonoBehaviour
         attitude3 = "Calm and Kind";
         position = POSITION.Backline;
         story = "A master of their trade from a faraway land.";
+    }
+
+    public void SaveCharacter()
+    {
+        var saveData = new SaveData
+        {
+            // Basic Info
+            raiderName = this.raiderName,
+            raiderRole = this.raiderRole,
+            raiderRace = this.raiderRace,
+            alignment = this.alignment,
+            looks1 = this.looks1,
+            looks2 = this.looks2,
+            looks3 = this.looks3,
+            attitude1 = this.attitude1,
+            attitude2 = this.attitude2,
+            attitude3 = this.attitude3,
+            position = this.position,
+            story = this.story,
+
+            // Equipment
+            primaryWeapon = primaryWeapon != null ? new WeaponData
+            {
+                weaponName = primaryWeapon.weaponName,
+                modifier = primaryWeapon.modifier,
+                rollNumber = primaryWeapon.rollNumber,
+                dice = primaryWeapon.dice,
+                weaponType = primaryWeapon.weaponType,
+                damageType = primaryWeapon.damageType
+            } : null,
+            secondaryWeapon = secondaryWeapon != null ? new WeaponData
+            {
+                weaponName = secondaryWeapon.weaponName,
+                modifier = secondaryWeapon.modifier,
+                rollNumber = secondaryWeapon.rollNumber,
+                dice = secondaryWeapon.dice,
+                weaponType = secondaryWeapon.weaponType,
+                damageType = secondaryWeapon.damageType
+            } : null,
+            armor = new ArmorData { armorName = armor.armorName, additionalArmorClass = armor.additionalArmorClass },
+            coreAugment = coreAugment != null ? new AugmentData
+            {
+                augmentName = coreAugment.augmentName,
+                augmentType = coreAugment.augmentType,
+                cooldownAmount = coreAugment.cooldownAmount,
+                cooldownInterval = coreAugment.cooldownInterval,
+                augmentDescription = coreAugment.augmentDescription
+            } : null,
+
+            // Stats & Skills
+            hpMax = this.hpMax,
+            hpCurrent = this.hpCurrent,
+            acTotal = this.acTotal,
+            acTemp = this.acTemp,
+            strength = this.strength,
+            intelligence = this.intelligence,
+            dexterity = this.dexterity,
+            credits = this.credits,
+
+            // Skills
+            acrobatics = this.acrobatics,
+            athletics = this.athletics,
+            database = this.database,
+            domination = this.domination,
+            eloquence = this.eloquence,
+            instinct = this.instinct,
+            sensory = this.sensory,
+            stealth = this.stealth,
+            technology = this.technology,
+
+            // Ship Parts & Appearance
+            hasPowerRegulator = this.hasPowerRegulator,
+            hasMainDrive = this.hasMainDrive,
+            hasStabilizerUnit = this.hasStabilizerUnit,
+            hasNavigationModule = this.hasNavigationModule,
+            hasShieldGenerator = this.hasShieldGenerator,
+            hasType2Portrait = this.hasType2Portrait
+        };
+
+        string json = JsonUtility.ToJson(saveData, true);
+        string path = Path.Combine(Application.persistentDataPath, $"{raiderName}_character.json");
+        File.WriteAllText(path, json);
+        Debug.Log($"Character saved to: {path}");
+    }
+
+    public void LoadCharacter(string characterName)
+    {
+        string path = Path.Combine(Application.persistentDataPath, $"{characterName}_character.json");
+        if (!File.Exists(path))
+        {
+            Debug.LogWarning($"No save file found at: {path}");
+            return;
+        }
+
+        string json = File.ReadAllText(path);
+        var saveData = JsonUtility.FromJson<SaveData>(json);
+
+        // Basic Info
+        this.raiderName = saveData.raiderName;
+        this.raiderRole = saveData.raiderRole;
+        this.raiderRace = saveData.raiderRace;
+        this.alignment = saveData.alignment;
+        this.looks1 = saveData.looks1;
+        this.looks2 = saveData.looks2;
+        this.looks3 = saveData.looks3;
+        this.attitude1 = saveData.attitude1;
+        this.attitude2 = saveData.attitude2;
+        this.attitude3 = saveData.attitude3;
+        this.position = saveData.position;
+        this.story = saveData.story;
+
+        // Equipment
+        this.primaryWeapon = saveData.primaryWeapon != null ?
+            new Weapon(saveData.primaryWeapon.weaponName, saveData.primaryWeapon.modifier,
+                      saveData.primaryWeapon.rollNumber, saveData.primaryWeapon.dice,
+                      saveData.primaryWeapon.weaponType, saveData.primaryWeapon.damageType) : null;
+
+        this.secondaryWeapon = saveData.secondaryWeapon != null ?
+            new Weapon(saveData.secondaryWeapon.weaponName, saveData.secondaryWeapon.modifier,
+                      saveData.secondaryWeapon.rollNumber, saveData.secondaryWeapon.dice,
+                      saveData.secondaryWeapon.weaponType, saveData.secondaryWeapon.damageType) : null;
+
+        this.armor = new Armor(saveData.armor.armorName, saveData.armor.additionalArmorClass);
+
+        this.coreAugment = saveData.coreAugment != null ?
+            new Augment(saveData.coreAugment.augmentName, saveData.coreAugment.augmentType,
+                       saveData.coreAugment.cooldownAmount, saveData.coreAugment.cooldownInterval,
+                       saveData.coreAugment.augmentDescription) : null;
+
+        // Stats & Skills
+        this.hpMax = saveData.hpMax;
+        this.hpCurrent = saveData.hpCurrent;
+        this.acTotal = saveData.acTotal;
+        this.acTemp = saveData.acTemp;
+        this.strength = saveData.strength;
+        this.intelligence = saveData.intelligence;
+        this.dexterity = saveData.dexterity;
+        this.credits = saveData.credits;
+
+        // Skills
+        this.acrobatics = saveData.acrobatics;
+        this.athletics = saveData.athletics;
+        this.database = saveData.database;
+        this.domination = saveData.domination;
+        this.eloquence = saveData.eloquence;
+        this.instinct = saveData.instinct;
+        this.sensory = saveData.sensory;
+        this.stealth = saveData.stealth;
+        this.technology = saveData.technology;
+
+        // Ship Parts & Appearance
+        this.hasPowerRegulator = saveData.hasPowerRegulator;
+        this.hasMainDrive = saveData.hasMainDrive;
+        this.hasStabilizerUnit = saveData.hasStabilizerUnit;
+        this.hasNavigationModule = saveData.hasNavigationModule;
+        this.hasShieldGenerator = saveData.hasShieldGenerator;
+        this.hasType2Portrait = saveData.hasType2Portrait;
+
+        Debug.Log($"Character loaded from: {path}");
     }
 
 }
